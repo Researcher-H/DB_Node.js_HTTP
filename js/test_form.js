831b442a731a95a6cb763531eea797c9a9368d2d
tsujimurah <researcher.hhh@gmail.com>
2020-09-28 05:33:02 +0100

    add GET func

--- js/test_form.js
+++ js/test_form.js
@@ -1,5 +1,5 @@
 // 
-// a vanilla javascript
+// vanilla javascript
 // 
 
 // quick test
@@ -11,6 +11,8 @@ var y = document.querySelectorAll("#js_data");
 y[0].innerText = "green";
 
 // 
+
+// 
 // jQuery
 // 
 
@@ -20,15 +22,67 @@ y[0].innerText = "green";
 
 // use jquery to GET data from the data folder
 var settings_GET = {
-  "url": "../data/data.json",
+  "url": "../file/data.json",
   "method": "GET",
   "timeout": 0,
+  "dataType": "json",
 };
 
 // function
-$.ajax(settings_GET).done(function (response) {
+$.ajax(settings_GET)
+.done(function(response){
   console.log(response);
+})
+.fail(function( xhr, status, errorThrown ) {
+  alert( "Sorry, there was a problem!" );
+  console.log( "Error: " + errorThrown );
+  console.log( "Status: " + status );
+  console.dir( xhr );
+})
+.always(function( xhr, status ) {
+  // Code to run regardless of success or failure;
+  console.log( "The request is complete!" );
 });
+
+// 
+
+// other useful sources
+// https://learn.jquery.com/ajax/jquery-ajax-methods/
+// https://www.tutorialsteacher.com/jquery/jquery-get-method
+// https://www.tutorialsteacher.com/codeeditor?cid=jquery-111
+// https://api.jquery.com/jQuery.get/
+
+// function 2
+
+// use jquery to GET data from the data folder
+var settings_GET2 = {
+  "url": "../file/steve.json",
+  "method": "GET",
+  "timeout": 0,
+  "dataType": "json",
+};
+
+// function
+$.ajax(settings_GET2)
+.done(function(response){
+  console.log(response);
+  $('#ajaxBtn').click(function(){
+    console.log(response[0].name);
+    $('#ajaxInput').append(response[0].name);
+  });
+})
+.fail(function( xhr, status, errorThrown ) {
+  alert( "Sorry, there was a problem!" );
+  console.log( "Error: " + errorThrown );
+  console.log( "Status: " + status );
+  console.dir( xhr );
+})
+.always(function( xhr, status ) {
+  // Code to run regardless of success or failure;
+  console.log( "The request is complete!" );
+});
+
+// 
 
 // POST
 
