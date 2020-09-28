59b338cac4008a066d428c16c4ec90419044c355
tsujimurah <researcher.hhh@gmail.com>
2020-09-28 04:48:26 +0100

    add jquery func

--- js/test_form.js
+++ js/test_form.js
@@ -1,3 +1,6 @@
+// 
+// a vanilla javascript
+// 
 
 // quick test
 var x = document.querySelectorAll("#y_highlight");
@@ -7,4 +10,37 @@ x[0].style.backgroundColor = "yellow";
 var y = document.querySelectorAll("#js_data");
 y[0].innerText = "green";
 
+// 
+// jQuery
+// 
+
+// use postman code
+
+// GET
+
+// use jquery to GET data from the data folder
+var settings_GET = {
+  "url": "../data/data.json",
+  "method": "GET",
+  "timeout": 0,
+};
+
+// function
+$.ajax(settings_GET).done(function (response) {
+  console.log(response);
+});
+
+// POST
+
+// // use jquery to POST data from the data folder
+// var settings_POST = {
+//   "url": "../file/file.json",
+//   "method": "POST",
+//   "timeout": 0,
+// };
+
+// // function
+// $.ajax(settings_POST).done(function (response) {
+//   console.log(response);
+// });
 
