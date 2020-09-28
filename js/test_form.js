0011c25d88ef3aa980a4a649a43123b44496bcfa
tsujimurah <researcher.hhh@gmail.com>
2020-09-28 03:28:30 +0100

    add

---
+++ js/test_form.js
@@ -0,0 +1,10 @@
+
+// quick test
+var x = document.querySelectorAll("#y_highlight");
+x[0].style.backgroundColor = "yellow";
+
+// test to add text
+var y = document.querySelectorAll("#js_data");
+y[0].innerText = "green";
+
+
